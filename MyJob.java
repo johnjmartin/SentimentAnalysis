9b8e3b01d84273ca31c6ee5cad49256b89ef5a47
John Martin <johnjosephmartin@icloud.com>
2016-12-11 13:18:15 -0500

    adding project files

---
+++ MyJob.java
@@ -0,0 +1,165 @@
+package cisc.mapreduce;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class MyJob extends Configured implements Tool {
+	public static Set<String> goodWords = new HashSet<String>();
+	public static Set<String> badWords = new HashSet<String>();
+    
+    public static class MapClass extends MapReduceBase
+        implements Mapper<LongWritable, Text, Text, Text> {
+    	
+    	private Text product_id = new Text();
+    	private Text body = new Text();
+    	
+        public void map(LongWritable key, Text value,
+                        OutputCollector<Text, Text> output,
+                        Reporter reporter) throws IOException {
+        	String[] gas = value.toString().split("\t");
+        	//Initially had errors thrown when using larger data file beacuse some lines did not have 8 entries
+        	//Checking length before accessing.. Also wrapped in a try/catch to be extra safe
+        	if (gas.length >= 8) {
+	        	try {
+		        	body.set(gas[1]);
+		        	product_id.set(gas[7]);
+	        	} catch (Exception e){
+	        		System.err.println("Caught exception while parsing line of data (data is inconsistent)");
+	        	}
+	            output.collect(body, product_id);
+        	}
+        }
+    }
+    
+    public static class Reduce extends MapReduceBase
+        implements Reducer<Text, Text, Text, Text> {
+    	
+        private Text result = new Text();
+
+        public void reduce(Text key, Iterator<Text> values,
+                           OutputCollector<Text, Text> output,
+                           Reporter reporter) throws IOException {
+            //Total sentiment words count
+        	int count = 0;
+            String[] csv;
+            //Iterating over each review -- if more than one
+            while (values.hasNext()) {
+            	//remove all non whitespace and non characters from review body, split by spaces between words 
+	            csv = values.next().toString().replaceAll("[^\\p{L}\\p{Z}]","").split(" "); 
+                for (String word : csv) {
+                	//Using HashSet because lookup is O(1) -- arraylist or similar is O(n) 
+                	if (goodWords.contains(word)) {
+                		count = count+1;
+                	}
+                	if (badWords.contains(word)) {
+                		count = count-1;
+                	}
+                }
+            }
+            if (count > 0) {
+                result.set("Positive Sentiment");            	
+            }
+            else if (count < 0) {
+            	result.set("Negative Sentiment");
+            }
+            else {
+            	result.set("Neutral Sentiment");
+            }
+            // output text of value rather than just the count of total sentiment
+            output.collect(key, result);
+        }
+    }
+    
+    public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
+        
+        JobConf job = new JobConf(conf, MyJob.class);
+    	positiveList(args[1]); //Path to positive words file
+    	negativeList(args[0]); //Path to negative words file
+        Path in = new Path(args[2]);
+        Path out = new Path(args[3]);
+        FileInputFormat.setInputPaths(job, in);
+        FileOutputFormat.setOutputPath(job, out);
+        
+        job.setJobName("MyJob");
+        job.setMapperClass(MapClass.class);
+        job.setReducerClass(Reduce.class);
+        
+        job.setInputFormat(TextInputFormat.class);
+        job.setOutputFormat(TextOutputFormat.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        
+        JobClient.runJob(job);
+        
+        return 0;
+    }
+
+    private void positiveList(String p) {
+   		try {
+   			BufferedReader fis = new BufferedReader(new FileReader(new File(p)));
+   			String word;
+   			while ((word = fis.readLine()) != null) {
+   				goodWords.add(word);
+   			}
+   			fis.close();
+   		} catch (IOException ioe) {
+  			System.err.println("Caught exception..File not found");
+   		}
+   	}
+
+   	private void negativeList(String p) {
+  		try {
+  			BufferedReader fis = new BufferedReader(new FileReader(new File(p)));
+  			String word;
+  			while ((word = fis.readLine()) != null) {
+  				badWords.add(word);
+  			}
+  			fis.close();
+  		} catch (IOException ioe) {
+  			System.err.println("Caught exception..File not found");
+  		}
+  	}
+    
+    public static void main(String[] args) throws Exception { 
+        int res = ToolRunner.run(new Configuration(), new MyJob(), args);
+        
+        System.exit(res);
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+#Sentiment Analysis
+##Files included
+  * fullOutput (output from mapreduce job)
+  * reducedOutput
+  * MyJob.jar
+  * MyJob.java
+  * README.md
+##Execution
+`./hadoop jar [path]/MyJob.jar /cisc432/negative.txt /cisc432/positive.txt /cisc432/data.txt [path]/final_output`
+##MapReduce Programming & Development Process
+My algorithm was to iterate over the bodies of every review for a product, matching each word to the provided list of positives and negatives. I kept a running counter starting at 0, adding 1 for each positive word matched, and subtracting 1 for each negative word. If there was more than one review for a product_id, I would carry over the value of the current count. This is essentially treats different reviews for a single product_id as one large review. I explain my thinking behind this later on. If after iterating through the whole review set the sum was > 0, = 0, or < 0 I decided the overall sentiment for the product was positive, neutral, or negative, respectively.
+
+Translating this to MapReduce, for the mapper I read a single line of the file at a time. Assuming the line is in the expected format (there were several lines that were not and had to be skipped), I send the product_id to output as the key, and the review body as the value. This is a low computation process and happens very quickly even on the larger dataset. In the next phase, my reducer aggregates all of the reviews that have the same product_id into the Iterator<Text> values of the reducer. In the reducer I use some simple text processing to apply my above algorithm for sentiment analysis.
+
+For job speed optimization, I tried several things. First I wrote my MapReduce so that the majority of computation happened within the reducer, where each partition has access to the list of bodies. This took approximately 3 minutes to run with the full dataset on Linux 4. Next, I tried to use a combiner at the end of each map, this made my code faster, but I was not able to get correct output from it. The combiner likely did not work because it did not have access to every single review body for a product and thus was not properly summing the count of each review body. Some details about my development and testing process with Hadoop.
+
+I started testing using the reduced dataset from Assignment 2, in my mapper I knew I wanted the key to be product_id, and the value to be the review body. It took a bit of work to get my Mapper Headers implementing the correct types before my map class worked. When I thought I was done my mapper I ran:
+“””./hadoop jar [path to jar] –D mapred.reduce.tasks=0 [input paths] [output path]’”””
+to view the output of just my mapper job. Once the mapper output was what I was expecting, I started on my reducer.
+
+My reducer took the product_id, and a Text Iterator for the review bodies. Using the iterator, I processed each text body to remove any symbols that were not characters or whitespace, then I split the review by whitespace, and calculated the total number of positives and negatives in the review body. I outputted “Positive Sentiment”, “Neutral Sentiment”, or “Negative Sentiment” depending on the total number of sentiment related words in each review. If there was more than one review I treated them as one large review, carrying the total sentiment count forward through each review iteration. I considered treating each review as +1 for positive, -1 for negative, and outputting that value. But it made more sense to me to take to count the total number of positive and negative words in each review because in that case if someone feels overwhelmingly for or against the product it is shown in the total count of the review sentiment.
+
+Lastly, I definitely enjoyed this assignment the most. While I found MapReduce challenging to learn at first, things definitely speed up once you understand the basic structure of development. After seeing my final results, it’s clear how powerful MapReduce is for consuming, mutating, and analyzing large datasets. There were 1,431,031 different reviews in the dataset, the fact that it took less than 3 minutes to analyze and aggregate every review is very impressive and really shows the power of Hadoop.
